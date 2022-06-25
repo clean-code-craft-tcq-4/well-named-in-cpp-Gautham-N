791f06738be45cc03f4582a36f69f826a9c707bc
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-06-25 15:34:57 +0530

    based on feedback

--- DisplayOutput.cpp
+++ DisplayOutput.cpp
@@ -3,7 +3,8 @@
 void DisplayOutput::displayPairMap()
 {
     std::cout << "Pair No" << " - " << "Color" << std::endl;
-    for (int number = 1; number <= 25; number++)
+    int ColorPairMapCnt = 25;
+    for (int number = 1; number <= ColorPairMapCnt; number++)
     {
         TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(number);
         std::cout << number << " - " << colorPair.ToString() << std::endl;
